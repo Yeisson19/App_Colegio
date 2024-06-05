de2b997d407fea53a61ae33d3971a0077e71ca17
X-Chiku-X <jesuspetitpimentel@gmail.com>
2024-06-04 23:16:39 -0400

    pagos 4/6

--- src/screens/Pagos.jsx
+++ src/screens/Pagos.jsx
@@ -2,34 +2,42 @@ import React, { useEffect, useState } from "react";
 import { FlatList, Text, View, StyleSheet, TouchableOpacity , RefreshControl } from "react-native";
 import axios from 'axios';
 import Constants from 'expo-constants'
-import { Ionicons } from '@expo/vector-icons';
+
 import RepositoryItem_pagos from '../components/RepositoryItem_pagos'
 import {BASE_URL} from '../services/url.jsx'
-import { useRoute } from '@react-navigation/native';
+import { AuthContext } from '../context/AuthContext';
 
-const Pagos = ({ route }) => {
-  // const route = useRoute();
-  const  userData  = route.params;
+
+
+const Pagos = () => {
+  const { token } = useContext(AuthContext);
   const [pagos, setPagos] = useState([]);
   const [isRefreshing, setIsRefreshing] = useState(false);
 
-  useEffect(() => {
-    fetchData();
-  }, [userData]);
 
   const fetchData = async () => {
     try {
       setIsRefreshing(true);
-      const response = await axios.post(`${BASE_URL}/api/mobile/Pagos.php`, {
-        dato: userData
+      const response = await axios.post(`${BASE_URL}/api/mobile/horario.php`, {
+        token: token
       });
-      setPagos(response.data);
+
+      console.log(response.data);
+      if (response.data.success) {
+        setPagos(response.data.resultado);
+      } else {
+        // console.log(response.data.msg);
+        Alert.alert('Error', response.data.msg || 'Error al obtener datos');
+      }
     } catch (error) {
       console.error('Error: ', error.message);
     } finally {
       setIsRefreshing(false);
     }
   };
+  useEffect(() => {
+    fetchData();
+  }, [token]);
 
   const handleRefresh = () => {
     fetchData();
