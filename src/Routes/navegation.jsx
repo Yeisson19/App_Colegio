df1253f29c9aba9df2783ee194dc1d7ea418ecf2
X-Chiku-X <jesuspetitpimentel@gmail.com>
2024-07-07 13:14:14 -0400

    menu

--- src/Routes/navegation.jsx
+++ src/Routes/navegation.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect,useState  } from 'react';
-import { Button , Alert } from 'react-native';
+import { Button , Alert, Image } from 'react-native';
 import { createDrawerNavigator, DrawerItem } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from '@expo/vector-icons'; // Importa el ícono de Ionicons
 import { AuthContext } from '../context/AuthContext';
-
+import homeIcon from '../screens/img/logo.png';
 // Screens
 import Home from '../screens/Home';
 import Materia from '../screens/Materia';
@@ -43,34 +43,96 @@ const NavDrawer = () => {
     <Drawer.Navigator 
     // initialRouteName="Seccion"
     >
-      <Drawer.Screen name="INICIO" component={Home} />
+      <Drawer.Screen
+        name="INICIO"
+        component={Home}
+        options={{
+          drawerIcon: () => (
+            <Image source={homeIcon} style={{ width: 24, height: 24 }} />
+          ),
+        }}
+      />
+
       <Drawer.Screen 
         name="Materia" 
         component={Materia} 
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'library' : 'library-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
-      <Drawer.Screen 
-        name="Seccion" 
-        component={Seccion} 
-        options={{ title: 'Seccion' }}
+      <Drawer.Screen
+        name="Seccion"
+        component={Seccion}
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'school' : 'school-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
       <Drawer.Screen 
         name="Horario" 
         component={horario} 
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'time' : 'time-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
         // initialParams={{ userData }} // Pasar solo userData
       />
       <Drawer.Screen 
         name="Pagos" 
         component={Pagos} 
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'card' : 'card-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
         // initialParams={{ userData }} // Pasar solo userData
       />
       <Drawer.Screen 
         name="Inscripciones" 
         component={Inscripciones} 
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
         // initialParams={{ userData }} // Pasar solo userData
       />
       <Drawer.Screen 
         name="Año Academico" 
         component={Ano_academico} 
+        options={{
+          drawerIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'calendar' : 'calendar-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
         // initialParams={{ userData }} // Pasar solo userData
       />
       <Drawer.Screen
