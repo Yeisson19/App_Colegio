e690d4979d923b099d4fd7eee203992790f4acf9
Legan2021 <jesusfob2021@gmail.com>
2024-07-17 06:13:51 -0400

    a

--- src/services/url.jsx
+++ src/services/url.jsx
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-export const BASE_URL= 'https://9dbd-38-137-177-201.ngrok-free.app/Nueva carpeta (3)/carlossoublette/';
-=======
-export const BASE_URL= 'https://e629-181-208-165-153.ngrok-free.app/carlossoublette';
->>>>>>> 94337b427c98516f0adbd96fb9d7e72415b1426d
+export const BASE_URL= 'https://64d8-38-137-177-201.ngrok-free.app/Nueva carpeta (3)/carlossoublette/';
+
