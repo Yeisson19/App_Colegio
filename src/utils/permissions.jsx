8655e5f4309f5883f3004cf282a69ee2a758bf8f
Yeisson19 <yeissoncolmenarez@gmail.com>
2024-07-20 05:58:09 -0400

    reporte pago y navegacion

--- src/utils/permissions.jsx
+++ src/utils/permissions.jsx
@@ -1,13 +1,13 @@
 export const rolePermissions = {
-    Tutor_Legal: ['Inicio', 'Pagos'],
+    Tutor: ['Inicio', 'Pagos'],
     Docente: ['Inicio', 'Seccion', 'Horario', 'Inscripciones', 'Año Academico'],
-    Admin: ['Inicio', 'Materia', 'Seccion', 'Horario', 'Pagos', 'Inscripciones', 'Año Academico','Reporte']
+    Admin: ['Inicio', 'Materia', 'Seccion', 'Horario', 'Pagos', 'Inscripciones', 'Año Academico','Reporte Inscripcion','Reporte Pago']
   };
 
   export const getUserRole = (role) => {
     switch (role) {
       case 1:
-        return { name: 'Tutor_Legal', permissions: rolePermissions.Tutor };
+        return { name: 'Tutor', permissions: rolePermissions.Tutor };
       case 19:
         return { name: 'Docente', permissions: rolePermissions.Docente };
       case 3:
